1f125b0f022e98fbf8c3fe94bc41577bd14c4cca
BoBppy <46050967+BoBppy@users.noreply.github.com>
2020-06-11 20:43:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@
 #============================================================
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 删除默认主题bootstrap，然而每次都执行失败，百思不得其解
 sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
