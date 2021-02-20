f69c35faf0d6cc989701953ae86ff0f5a3f51896
BoBppy <46050967+BoBppy@users.noreply.github.com>
2021-02-20 10:05:40 +0800

    fix actions
    switch argon to argon18.06 by jerrk

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,5 +11,5 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# 删除默认主题bootstrap，然而每次都执行失败，百思不得其解
-sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
+# 切换默认主题
+sed -i 's/bootstrap/argon-18.06/g' feeds/luci/collections/luci/Makefile
